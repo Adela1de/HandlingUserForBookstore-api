ec7a076af6fd2631a5b23d97d4af777ffba3ac17
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-08-05 10:58:26 -0300

    Default Security configuration

---
+++ Oauth-authorization-server/Oauth-authorization-server/src/main/java/com/example/oauthserver/config/DefaultSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.oauthserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class DefaultSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception
+    {
+        http.authorizeHttpRequests( authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated() )
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+}
